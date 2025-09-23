e3d2bf491e3689e01f27ce0cfcf5d7508dca8619
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-23 11:41:45 +0530

    get , post operation done successfully

--- backend/controller.js
+++ backend/controller.js
@@ -37,7 +37,7 @@ const UserById = async (req,res) => {
         res.status(404).json({message : "user not found"});
      }
      else {
-        res.status(userid);
+        res.status(200).json(userid);
      }
    }
    catch (error) {
